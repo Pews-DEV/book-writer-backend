3be11ea229a0e5bd4921aa9e947e2ff1071fdf84
michaelpereira31 <michaelpereira408@gmail.com>
2022-04-09 19:40:59 -0300

    refactor: applying new configuration patterns

--- app/index.ts
+++ app/index.ts
@@ -1,13 +1,13 @@
 import express = require('express');
 import routers from './routers';
 
-import './src/database'
+import './src/database';
 
 const app = express();
 
 app.use(express.json());
-app.use(routers)
+app.use(routers);
 
 app.listen(3333, () => {
-    console.log('Servidor rodando')
+    console.log('Servidor rodando');
 });
