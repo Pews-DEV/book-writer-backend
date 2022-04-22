ece800d34e37e0a970ebd4dc85e9abe9b5c684cc
José Pedro <pedroosd28@gmail.com>
2022-04-21 22:53:16 -0300

    refactor: exporting function to improve readability

--- app/modules/user/validations/email.ts
+++ app/modules/user/validations/email.ts
@@ -11,24 +11,24 @@ const EMAIL_LENGTH_ERROR_MESSAGE = UserRepository.LENGTH_ERROR_MESSAGE
   .replace(FIELD_VAR, 'email')
   .replace(LENGTH_VAR, '5')
 
-const email = getBodyField('email')
+function checkEmailIsUnique(value) {
+  const userRepository = getCustomRepository(UserRepository)
+  const isUnique = userRepository
+    .checkIsUnique('email', value)
+    .then(isUnique => {
+      if (!isUnique) {
+        return Promise.reject()
+      }
+      return Promise.resolve()
+    })
+  return isUnique
+}
 
+const email = getBodyField('email')
 const emailValidation = email
   .isEmail().withMessage(INVALID_EMAIL_MESSAGE)
   .isLength({ min: 5 }).withMessage(EMAIL_LENGTH_ERROR_MESSAGE)
-  .custom((value) => {
-    const userRepository = getCustomRepository(UserRepository)
-    const isUnique = userRepository
-      .checkIsUnique('email', value)
-      .then(res => {
-        if (!res) {
-          return Promise.reject()
-        }
-        return Promise.resolve()
-      })
-
-    return isUnique
-  }).withMessage(NON_UNIQUE_ERROR_MESSAGE)
+  .custom(checkEmailIsUnique).withMessage(NON_UNIQUE_ERROR_MESSAGE)
 
 
 export default emailValidation
--- app/modules/user/validations/username.ts
+++ app/modules/user/validations/username.ts
@@ -10,22 +10,22 @@ const LENGHT_ERROR_MESSAGE = UserRepository.LENGTH_ERROR_MESSAGE
   .replace(FIELD_VAR, 'username')
   .replace(LENGTH_VAR, '1')
 
-const username = getBodyField('username')
+function checkUsernameIsUnique(value) {
+  const userRepository = getCustomRepository(UserRepository)
+  const isUnique = userRepository
+    .checkIsUnique('username', value)
+    .then(isUnique => {
+      if (!isUnique) {
+        return Promise.reject()
+      }
+      return Promise.resolve()
+    })
+  return isUnique
+}
 
+const username = getBodyField('username')
 const usernameValidation = username
   .isLength({min: 1}).withMessage(LENGHT_ERROR_MESSAGE)
-  .custom((value) => {
-    const userRepository = getCustomRepository(UserRepository)
-    const isUnique = userRepository
-      .checkIsUnique('username', value)
-      .then(res => {
-        if (!res) {
-          return Promise.reject()
-        }
-        return Promise.resolve()
-      })
-
-    return isUnique
-  }).withMessage(NON_UNIQUE_ERROR_MESSAGE)
+  .custom(checkUsernameIsUnique).withMessage(NON_UNIQUE_ERROR_MESSAGE)
 
 export default usernameValidation
