82af746d628eac5fe4481bfe0e0ec0e45dc3b2ba
José Pedro <pedroosd28@gmail.com>
2022-04-22 14:18:40 -0300

    build: adding coverage limit

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,18 @@
-/*
- * For a detailed explanation regarding each configuration property and type check, visit:
- * https://jestjs.io/docs/configuration
- */
+const COVERAGE_EXPECTATION = process.env.NODE_ENV === 'CI' ? 70 : 0;
 
 export default {
   roots: ['<rootDir>/app'],
   collectCoverageFrom: ['<rootDir>/app/**/*.ts'],
   coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'text-summary', 'cobertura', 'lcov'],
+  coverageThreshold: {
+    global: {
+      branches: COVERAGE_EXPECTATION,
+      functions: COVERAGE_EXPECTATION,
+      lines: COVERAGE_EXPECTATION,
+      statements: COVERAGE_EXPECTATION,
+    },
+  },
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest',
