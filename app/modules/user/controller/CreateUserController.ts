469d0629a7cb722469aeb73de51c8d287c8a3b2e
José Pedro <pedroosd28@gmail.com>
2022-04-24 16:55:10 -0300

    refactor: improving use of yup lib

--- app/modules/user/controller/CreateUserController.ts
+++ app/modules/user/controller/CreateUserController.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from 'express';
 import * as yup from 'yup';
 
+import yupErrorHandle from '@/utils/yup/errorHandler';
+import validateSchema from '@/utils/yup/validateSchema';
 import CreateUserService from '../services/CreateUser';
 import checkFieldExist from '../utils/checkFieldExist';
 
-export default class UserController {
+export default class CreateUserController {
   public static readonly schema = yup.object({
     body: yup.object({
       first_name: yup.string().required('O first_name é obrigatório'),
@@ -32,9 +34,7 @@ export default class UserController {
 
   async handle(request: Request, response: Response): Promise<Response> {
     try {
-      await UserController.schema.validate(request, {
-        abortEarly: false,
-      });
+      await validateSchema(CreateUserController.schema, request);
       const userData = request.body;
       const createUser = new CreateUserService();
       const user = await createUser.execute(userData);
@@ -45,14 +45,7 @@ export default class UserController {
         data: user,
       });
     } catch (err) {
-      return response.status(422).json({
-        status_code: 422,
-        success: false,
-        errors: err.inner.map((error: yup.ValidationError) => ({
-          error: error.errors[0],
-          param: error.path,
-        })),
-      });
+      return response.status(422).json(yupErrorHandle(err, 422));
     }
   }
 }
