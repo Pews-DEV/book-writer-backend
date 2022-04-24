0b4123e3ed6de6207a5c1258aa24ff3805c808dd
José Pedro <pedroosd28@gmail.com>
2022-04-24 16:08:19 -0300

    fix: renaming checkExists to checkFieldExists

--- app/modules/user/utils/checkFieldExist.ts
+++ app/modules/user/utils/checkFieldExist.ts
@@ -2,9 +2,9 @@ import { getCustomRepository } from 'typeorm';
 
 import UserRepository from '../repository/UserRepository';
 
-const checkExist = async (columnName: string, value: string) => {
+const checkFieldExist = async (columnName: string, value: string) => {
   const userRepository = getCustomRepository(UserRepository);
-  return await userRepository.checkExist(columnName, value);
+  return await userRepository.checkFieldExist(columnName, value);
 };
 
-export default checkExist;
+export default checkFieldExist;
