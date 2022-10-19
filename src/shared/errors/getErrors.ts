5bd4b4c639d29603507fc353f3688fd1e95c77dc
Michael Pereira <michael.pereira@chat2desk.com.br>
2022-10-19 12:33:19 -0300

    feat: dealing with errors

---
+++ src/shared/errors/getErrors.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express';
+
+import { AppError } from './AppError';
+
+export function getErrors(
+  error: Error,
+  request: Request,
+  response: Response,
+  _: NextFunction,
+): Response {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+}
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
+import { getErrors } from '@shared/errors/getErrors';
 import { logger } from '@shared/providers/logger/implementations/LoggerProvider';
 
 const app = express();
 
 app.use(express.json());
+app.use(getErrors);
 
 app.listen(3333, () => logger.info('Server is running on port 3333'));
