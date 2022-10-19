893394e0415be5b913328a0694c58a6024c049f5
Michael Pereira <michael.pereira@chat2desk.com.br>
2022-10-19 15:21:57 -0300

    feat: add methods in user

--- src/modules/user/repositories/IUserRepository.ts
+++ src/modules/user/repositories/IUserRepository.ts
@@ -1,7 +1,12 @@
 import { User } from '@prisma/client';
 
 import { ICreateUserDTO } from '../dtos/ICreateUserDTO';
+import { IUpdateUserDTO } from '../dtos/IUpdateUserDTO';
 
 export type IUserRepository = {
+  findById(id: string): Promise<User | null>;
+  findByEmail(email: string): Promise<User | null>;
   create(params: ICreateUserDTO): Promise<User>;
+  update(params: IUpdateUserDTO): Promise<User>;
+  delete(id: string): Promise<void>;
 };
