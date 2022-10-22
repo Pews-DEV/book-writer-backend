ac12fc0fdc815d171e1a41ec3b6262421b197530
Michael Pereira <michael.pereira@chat2desk.com.br>
2022-10-22 15:08:01 -0300

    feat: add middleware is authenticated

---
+++ src/shared/infra/http/middlewares/isAuthenticated.ts
@@ -0,0 +1,36 @@
+import { Request } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import { jwtConfig } from '@config/jwtConfig';
+import { AppError } from '@shared/errors/AppError';
+
+type ITokenPayload = {
+  userName: string;
+  email: string;
+  sub: string;
+};
+export function isAuthenticated(request: Request): void {
+  const authenticate = request.headers.authorization;
+
+  if (!authenticate) {
+    throw new AppError('Token in missing');
+  }
+
+  const [, token] = authenticate.split(' ');
+
+  try {
+    const {
+      sub: user,
+      userName,
+      email,
+    } = verify(token, jwtConfig.secretKey) as ITokenPayload;
+
+    request.user = {
+      id: user,
+      email,
+      userName,
+    };
+  } catch {
+    throw new AppError('Invalid token');
+  }
+}
--- src/shared/infra/http/routes/users.router.ts
+++ src/shared/infra/http/routes/users.router.ts
@@ -1,19 +1,24 @@
 import { Router } from 'express';
 
+import { AuthenticateController } from '@modules/authenticate/useCases/authenticateUser/AuthenticateController';
 import { CreateUserController } from '@modules/user/useCases/createUser/CreateUserController';
 import { DeleteUserController } from '@modules/user/useCases/deleteUser/DeleteUserController';
 import { FindUserByIdController } from '@modules/user/useCases/findUserById/FindUserByIdController';
 import { UpdateUserController } from '@modules/user/useCases/updateUser/UpdateUserController';
 
+import { isAuthenticated } from '../middlewares/isAuthenticated';
+
 const userRoutes = Router();
 
 const findUserByIdController = new FindUserByIdController();
 const createUserController = new CreateUserController();
+const authenticateController = new AuthenticateController();
 const updateUserController = new UpdateUserController();
 const deleteUserController = new DeleteUserController();
 
-userRoutes.get('/:id', findUserByIdController.handle);
+userRoutes.get('/:id', isAuthenticated, findUserByIdController.handle);
 userRoutes.post('/', createUserController.handle);
+userRoutes.post('/authenticate', authenticateController.handle);
 userRoutes.put('/:id', updateUserController.handle);
 userRoutes.delete('/:id', deleteUserController.handle);
 
