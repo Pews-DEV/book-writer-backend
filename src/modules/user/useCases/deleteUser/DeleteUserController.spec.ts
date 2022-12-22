bc3ee56b6e45eb33b0cacc2bb3073c0f2c1eeff9
Michael Pereira <michael.pereira@chat2desk.com.br>
2022-12-22 17:09:34 -0300

    test: add test in delete user

---
+++ src/modules/user/useCases/deleteUser/DeleteUserController.spec.ts
@@ -0,0 +1,57 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { mock, MockProxy } from 'jest-mock-extended';
+import { container } from 'tsyringe';
+
+import { IUserRepository } from '@modules/user/repositories/IUserRepository';
+
+import { DeleteUserController } from './DeleteUserController';
+import { DeleteUserUseCase } from './DeleteUserUseCase';
+
+const tsringeContainerMock = jest.spyOn(container, 'resolve');
+describe('DeleteUserController', () => {
+  let userRepository: MockProxy<IUserRepository>;
+  let sut: DeleteUserController;
+
+  beforeEach(() => {
+    userRepository = mock();
+    userRepository.findById.mockResolvedValue({
+      id: 'any_id',
+      firstName: 'any_first_name',
+      lastName: 'any_last_name',
+      email: 'any_email',
+      userName: 'any_user_name',
+      password: 'any_password',
+      isAdmin: true,
+      created_at: new Date('2022-08-23T17:33:38.232Z'),
+      updated_at: new Date('2022-08-23T17:33:38.232Z'),
+    });
+    userRepository.delete.mockResolvedValue();
+    tsringeContainerMock.mockImplementation(() => ({
+      execute: (id: string) => {
+        const deleteUserUseCase = new DeleteUserUseCase(userRepository);
+        return deleteUserUseCase.execute(id);
+      },
+    }));
+    sut = new DeleteUserController();
+  });
+  it('should be able execute controller for delete', async () => {
+    const request: any = {
+      user: jest.fn(),
+    };
+
+    const statusResponse = {
+      json: jest.fn(),
+    };
+
+    const response: any = {
+      json: jest.fn(),
+      status: jest.fn(() => statusResponse),
+    };
+
+    await sut.handle(request, response);
+    expect(response.status).toBeCalledWith(200);
+    expect(response.status().json).toBeCalledWith({
+      message: 'User deleted success',
+    });
+  });
+});
---
+++ src/modules/user/useCases/deleteUser/DeleteUserUseCase.spec.ts
@@ -0,0 +1,42 @@
+import { mock, MockProxy } from 'jest-mock-extended';
+
+import { IUserRepository } from '../../repositories/IUserRepository';
+import { DeleteUserUseCase } from './DeleteUserUseCase';
+
+describe('DeleteUserUseCase', () => {
+  let userRepository: MockProxy<IUserRepository>;
+  let sut: DeleteUserUseCase;
+
+  beforeEach(() => {
+    userRepository = mock();
+    userRepository.findById.mockResolvedValue({
+      id: 'any_id',
+      firstName: 'any_first_name',
+      lastName: 'any_last_name',
+      email: 'any_email',
+      userName: 'any_user_name',
+      password: 'any_password',
+      isAdmin: true,
+      created_at: new Date('2022-08-23T17:33:38.232Z'),
+      updated_at: new Date('2022-08-23T17:33:38.232Z'),
+    });
+    userRepository.delete.mockResolvedValue();
+    sut = new DeleteUserUseCase(userRepository);
+  });
+  it('should be able delete user', async () => {
+    await sut.execute('any_id');
+    expect(userRepository.delete).toHaveBeenCalledWith('any_id');
+    expect(userRepository.delete).toHaveBeenCalledTimes(1);
+  });
+
+  it('should be able to return error when user does not exist', async () => {
+    userRepository.findById.mockResolvedValue(null);
+    const request = async () => {
+      await sut.execute('any_id');
+    };
+    expect(request).rejects.toEqual({
+      message: 'User not found',
+      statusCode: 400,
+    });
+  });
+});
